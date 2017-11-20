3f6dfe72c5336da08c711c879e6678402ae00510
Fredrik Olsson <spooky.bender@gmail.com>
2017-11-20 08:32:41 +0100

    notes

--- malloc.h
+++ malloc.h
@@ -8,4 +8,6 @@ bool sp_free(void *) noexcept;
 std::size_t sp_usable_size(void *) noexcept;
 void *sp_realloc(void *, std::size_t) noexcept;
 
+//TODO calloc and memalign and posix_memalign?
+
 #endif
